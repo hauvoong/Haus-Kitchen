b66407220927aa9d2cc5675818d0fb8d727c5626
hauvoong <hauvoong@gmail.com>
2026-01-03 13:21:47 +0000

    update debug

--- hk_recipes/settings.py
+++ hk_recipes/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)*9+nsjq*70a9uq$)tpe#a(i%%l9j-u=z_e0@^znb_1k^=$)2h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.herokuapp.com',
                  '127.0.0.1',]
