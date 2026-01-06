e96c49c5c06df2be6a900ac2e2967e6f8521baa4
hauvoong <hauvoong@gmail.com>
2026-01-06 12:16:32 +0000

    Add Rating and Favourite Model

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Recipe, Comment
+from .models import Recipe, Comment, Favourite, Rating
 
 # Register your models here.
 admin.site.register(Recipe)
 admin.site.register(Comment)
+admin.site.register(Favourite)
+admin.site.register(Rating)
--- recipes/models.py
+++ recipes/models.py
@@ -32,3 +32,28 @@ class Comment(models.Model):
 
     def __str__(self):
         return f"Comment from {self.author} on {self.recipe.title} recipe"
+    
+
+class Favourite(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='favourites')
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name='favourited_by')
+    added_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user', 'recipe')
+
+    def __str__(self):
+        return f"{self.recipe.title} recipe has been favourited by {self.user}"
+    
+
+class Rating(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='ratings')
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name='ratings')
+    score = models.PositiveSmallIntegerField(choices=[(i, str(i)) for i in range(1, 6)])
+    rated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        unique_together = ('user', 'recipe')
+
+    def __str__(self):
+        return f"{self.user} rated {self.recipe.title} as {self.score}/5"
