a25f7965e9964cf37e69e3c4bb77ce1b2ce5f417
hauvoong <hauvoong@gmail.com>
2026-01-09 14:57:53 +0000

    Add authentication

--- hk_recipes/urls.py
+++ hk_recipes/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("recipes.urls"), name="recipes-urls"),
     path('summernote/', include('django_summernote.urls')),
+    path("accounts/", include("allauth.urls")),
 ]
