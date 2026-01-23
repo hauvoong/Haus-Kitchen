f132c2da874b4c747df02fd6d1f8f567b4231cdb
hauvoong <hauvoong@gmail.com>
2026-01-23 11:01:14 +0000

    Add max character for comments

--- recipes/models.py
+++ recipes/models.py
@@ -30,7 +30,7 @@ class Comment(models.Model):
         Recipe, on_delete=models.CASCADE, related_name='comments')
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name='commenter')
-    content = models.TextField(max_length=200)
+    content = models.TextField(max_length=400)
     created_at = models.DateTimeField(auto_now_add=True)
     approved = models.BooleanField(default=False)
 
